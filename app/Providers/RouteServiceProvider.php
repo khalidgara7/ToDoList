c75dc8e82207b2a0e5a446c756113ca1a2cbedae
khalid-elmati <elmatikhalid17@gmail.com>
2025-11-05 12:16:15 +0100

    Refactor RouteServiceProvider to streamline route registration and add JWT authentication guard in auth configuration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
@@ -23,18 +24,16 @@ class RouteServiceProvider extends ServiceProvider
      * Define your route model bindings, pattern filters, and other route configuration.
      */
     public function boot(): void
-    {
-        $this->configureRateLimiting();
+{
+    $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+    Route::middleware('api')
+        ->prefix('api')
+        ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
-    }
+    Route::middleware('web')
+        ->group(base_path('routes/web.php'));
+}
 
     /**
      * Configure the rate limiters for the application.
